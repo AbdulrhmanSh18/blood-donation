2a15ef16e5742f043407328ca31060751c5ef372
Abdulrhman Abuhyyh <Abdulrhman.Abuhyyh.jiz.s@tuwaiq.edu.sa>
2021-12-23 16:29:18 +0300

    init xcode project structuur

---
+++ blood-donation/blood-donation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  blood-donation
+//
+//  Created by Abdulrhman Abuhyyh on 19/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
